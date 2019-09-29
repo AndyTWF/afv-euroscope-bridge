6bb164333df49460ba9bd8855fbd4c22db2c35bb
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-09-29 02:12:41 +0100

    Version Bump

--- constants.h
+++ constants.h
@@ -1,6 +1,6 @@
 #pragma once
 
 #define PLUGIN_TITLE "Audio for Vatsim EuroScope Bridge"
-#define PLUGIN_VERSION "1.1.1"
+#define PLUGIN_VERSION "1.2.0"
 #define PLUGIN_AUTHOR "Audio for Vatsim Team"
 #define PLUGIN_COPYRIGHT "Virtual Air Traffic Simulation Network"
