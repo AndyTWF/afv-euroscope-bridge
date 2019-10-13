6b10a9de2f7216c3787fe569020da4f6bbcbecfd
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-10-14 00:43:40 +0100

    Code Tidying

--- HiddenWindow.cpp
+++ HiddenWindow.cpp
@@ -9,15 +9,15 @@ LRESULT CALLBACK HiddenWindow(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
     switch (msg) {
         case WM_CREATE : {
             bridge = reinterpret_cast<AfvBridge*>(reinterpret_cast<CREATESTRUCT*>(lParam)->lpCreateParams);
-            bridge->OnTimer(1);
             return TRUE;
         }
         case WM_COPYDATA: {
             COPYDATASTRUCT * data = reinterpret_cast<COPYDATASTRUCT *>(lParam);
 
-            if (data->dwData == 666 && bridge != nullptr) {
+            if (data != nullptr && data->dwData == 666 && data->lpData != nullptr && bridge != nullptr) {
                 bridge->AddMessageToQueue(reinterpret_cast<const char*>(data->lpData));
             }
+            return TRUE;
         }
     }
 
