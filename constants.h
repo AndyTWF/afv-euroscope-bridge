74a1c798d6b471f9d496caf39730b1df29d3fbfc
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-11-10 18:32:40 +0000

    Fix Multiple Messages

--- constants.h
+++ constants.h
@@ -1,6 +1,6 @@
 #pragma once
 
 #define PLUGIN_TITLE "Audio for Vatsim EuroScope Bridge"
-#define PLUGIN_VERSION "1.2.3"
+#define PLUGIN_VERSION "1.3.0"
 #define PLUGIN_AUTHOR "Audio for Vatsim Team"
 #define PLUGIN_COPYRIGHT "Virtual Air Traffic Simulation Network"
