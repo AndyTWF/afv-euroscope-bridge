a4fea38a90ff665ea96edde02e5c61963a123a2f
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-04-26 15:53:19 +0100

    Bump Version To Major
    This is a big BC breaker...

--- constants.h
+++ constants.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define PLUGIN_TITLE "Audio for Vatsim EuroScope Bridge"
-#define PLUGIN_VERSION "1.4.0"
+#define PLUGIN_VERSION "2.0.0"
 #define PLUGIN_AUTHOR "Audio for Vatsim Team"
 #define PLUGIN_COPYRIGHT "Virtual Air Traffic Simulation Network"
 #define HIDDEN_WINDOW_CLASS L"AfvBridgeHiddenWindowClass"
