c7424806a1e766de4cb79a73d0958a015795ca3b
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-04-26 16:52:29 +0100

    Change AFV Classname

--- constants.h
+++ constants.h
@@ -5,4 +5,4 @@
 #define PLUGIN_AUTHOR "Audio for Vatsim Team"
 #define PLUGIN_COPYRIGHT "Virtual Air Traffic Simulation Network"
 #define HIDDEN_WINDOW_CLASS L"AfvBridgeHiddenWindowClass"
-#define AFV_HIDDEN_WINDOW_CLASS L"AFVControllerClient"
+#define AFV_HIDDEN_WINDOW_CLASS L"WindowsForms10.Window.8.app.0.2fbafd8_r10_ad1"
